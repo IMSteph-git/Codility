bd85c6a54b0e5b7dce02be1d93ddd821e48b7613
IMSteph-git <stefan_ianuli@yahoo.com>
2024-02-10 21:52:09 +0200

    Added a comment

--- frog_river_one.py
+++ frog_river_one.py
@@ -2,9 +2,13 @@ import numpy as np
 
 def solution(X, A):
     
+    if(len(A)==1 ):
+        return 1
     sorted_a = list(set(sorted(A)))
     if(len(sorted_a) < X):
         return -1
+    if(len(A)==1 ):
+        return 1
     
     k = 0
     for i in A:
@@ -13,7 +17,7 @@ def solution(X, A):
             while i in A and k < X:
                 
                 A = [0 if value == i else value for value in A]
-                print(k,i,A)
+                #print(k,i,A)
             k += 1
         if k == X:
             index_array = [index for index, value in enumerate(A) if value != 0]
@@ -26,3 +30,4 @@ xx = 1
 AA = [1,3,1,4,2,3,5,4]
 B = [1]
 print(solution(xx,B) )
+#something
